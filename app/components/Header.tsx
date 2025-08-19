a54ab6106d5fd2160679c2d87ba0fd4a19b45f73
Bakarray <magnificentzeem@gmail.com>
2025-08-19 19:45:27 +0100

    portfolio complete

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import { assets } from "@/assets/assets";
+import { motion } from "motion/react";
 
 const Header = () => {
   return (
     <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
-      <div>
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
+      >
         <Image src={assets.profile_img} alt="" className="rounded-full w-40" />
-      </div>
+      </motion.div>
+
       <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
         Hi! I&apos;m Abdulazeem Bakare{" "}
         <Image src={assets.hand_icon} alt="" className="w-6" />
@@ -21,7 +27,7 @@ const Header = () => {
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
         <a
           href="#contact"
-          className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2"
+          className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
         >
           Contact me{" "}
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
@@ -29,7 +35,7 @@ const Header = () => {
         <a
           href="/sample-resume.pdf"
           download
-          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
           My resume <Image src={assets.download_icon} alt="" className="w-4" />
         </a>
